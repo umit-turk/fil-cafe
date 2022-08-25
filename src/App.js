3b964f495a1b0e2f728f26e29ceb9a2a83f2dd09
Umit Turk <uyt.turk@gmail.com>
2022-08-25 23:04:01 +0300

    arrows

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Categories from "./Categories";
 import items from "./data";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faInstagram} from '@fortawesome/free-brands-svg-icons'
-import image from './instagram.jpg'
-
+import image from "./instagram.jpg";
 
 import { IconContext } from "react-icons";
 const allCategories = [...new Set(items.map((item) => item.category))];
@@ -44,17 +41,29 @@ const App = () => {
         <main className="menu section">
           <div className="navbar">
             <li style={{ marginTop: 20, marginLeft: 20 }}>
-              <FaIcons.FaBars style={{width:22, height:22}} onClick={showSidebar} />
+              <FaIcons.FaBars
+                style={{ width: 22, height: 22 }}
+                onClick={showSidebar}
+              />
             </li>
           </div>
           <nav className={sideBar ? "nav-menu active" : "nav-menu"}>
-            <ul className="nav-menu-items" >
+            <ul className="nav-menu-items">
               <li className="navbar-toggle">
                 <li className="menu-bars" onClick={showSidebar}>
                   <AiIcons.AiOutlineClose />
                 </li>
               </li>
-              <a href="https://www.instagram.com/coffeefil/" style={{display:"flex", justifyContent:"space-between",marginLeft:20,marginRight:20,alignItems:"center"}}>
+              <a
+                href="https://www.instagram.com/coffeefil/"
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginLeft: 20,
+                  marginRight: 20,
+                  alignItems: "center",
+                }}
+              >
                 <h4>İnstagram</h4>
                 <img src={image} alt="" width={30} height="30" />
               </a>
@@ -62,10 +71,16 @@ const App = () => {
           </nav>
           <section className="menu section">
             <div className="title">
-              {/*  <img src={logo} alt="logo" className="logo" /> */}
               <h2>Menu</h2>
               <div className="underline"></div>
             </div>
+            <div
+              className="arrows"
+            >
+              <AiIcons.AiOutlineArrowLeft />
+              <AiIcons.AiOutlineArrowRight />
+            </div>
+
             <Categories
               categories={categories}
               activeCategory={activeCategory}
